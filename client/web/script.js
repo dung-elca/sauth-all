df288ea55fef7e1633a643d1d3a13e231e1cb75b
DUNGNGUYEN <dung.nguyenlequoc@elca.vn>
2025-08-06 15:51:12 +0700

    update

--- client/web/script.js
+++ client/web/script.js
@@ -7,6 +7,18 @@ document.getElementById("verifyBtn").onclick = function () {
     body: JSON.stringify({ lot_id: "your_lot_id_here" }),
   })
     .then((res) => res.json())
-    .then((data) => alert("Result " + JSON.stringify(data)))
+    .then((url) => {
+      const modal = document.getElementById("fullscreen-modal");
+      const iframe = document.getElementById("modal-iframe");
+      iframe.src = url;
+      modal.style.display = "flex";
+    })
     .catch((err) => alert("Error: " + err));
+};
+
+document.getElementById("close-modal").onclick = function () {
+  const modal = document.getElementById("fullscreen-modal");
+  modal.style.display = "none";
+  const iframe = document.getElementById("modal-iframe");
+  iframe.src = "";
 };
